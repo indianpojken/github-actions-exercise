2f50956354e182163ccc7084c51b5e02dbfe8c6e
Fredrik <io.qrdr@gmail.com>
2023-10-11 19:05:05 +0200

    title + anim

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,3 +1,5 @@
+import { useTransition, animated } from '@react-spring/web';
+
 import TodoListItem from './TodoListItem/TodoListItem.tsx';
 
 import type { Todo } from '../../types/types.ts';
@@ -11,16 +13,26 @@ interface Props {
 }
 
 export default function TodoList({ items, onToggle, onDelete }: Props) {
+  const transitions = useTransition(items, {
+    key: (item: Todo) => `${item.text}-${item.id}`,
+    from: { opacity: 0 },
+    enter: { opacity: 1, x: 0 },
+    leave: { opacity: 0, x: 400 },
+    config: { duration: 250 },
+  });
+
   return (
     <article className="todo-list">
-      {items.map((item) => (
-        <TodoListItem
-          key={`${item.text}-${item.id}`}
-          text={item.text}
-          completed={item.completed}
-          onToggle={() => onToggle(item.id)}
-          onDelete={() => onDelete(item.id)}
-        />
+      {transitions((style, item) => (
+        <animated.article style={style}>
+          <TodoListItem
+            key={`${item.text}-${item.id}`}
+            text={item.text}
+            completed={item.completed}
+            onToggle={() => onToggle(item.id)}
+            onDelete={() => onDelete(item.id)}
+          />
+        </animated.article>
       ))}
     </article>
   );
